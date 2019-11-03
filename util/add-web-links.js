8d75afcf2d0dd196476342ebfe49db168d951c76
erkangz <erkang.zheng@lifeomic.com>
2019-11-03 10:31:34 -0500

    Improve add-web-links script with command line options

--- util/add-web-links.js
+++ util/add-web-links.js
@@ -4,19 +4,62 @@
  * used in the Compliance app for each mapped requirement/control, instead of a
  * URL linking to the JupiterOne Policies app.
  *
- * You will need to update the `baseUrl` as well as the `procedure.webLink`
- * pattern to create the desired output.
+ * The program is pre-configured with URL patterns for SharePoint and Confluence
+ * using the name of each policy and procedure. Update the URL pattern as needed.
+ *
+ * Prerequisite: Node.js version 10 or later.
  */
 
 'use strict';
 
-const fs = require('fs');
+const error = require('../lib/error');
+const program = require('commander');
 
+program
+  .version(require('../package').version, '-v, --version')
+  .usage('[options]')
+  .option('-s, --site [SharePoint|Confluence]', 'the site where policies and procedures are hosted (only SharePoint and Confluence are supported by default)')
+  .option('-d, --domain [name]', 'company domain name or vanity subdomain name as part of the site URL')
+  .option('-k, --key [directoryNameOrSpaceKey]', 'Subdirectory name or key for the site, such as SPACEKEY for a Confluence site')
+  .option('-s, --replace-space [char]', 'Replace space in URL with this specified character')
+  .option('-c, --config [file]', 'JSON config file')
+  .option('-p, --param [name|id]', 'use either "name" or "id" from each policy/procedure to build the URL')
+  .parse(process.argv);
+
+const fs = require('fs');
 const configFile = 'templates/config.json';
 
-const baseUrl = 'https://company.sharepoint.com/Documents';
+const domain = program.domain || 'company';
+const site = program.site || 'default';
+const key = program.key;
+const spaceChar = program.replaceSpace || '';
+const param = program.param || (site === 'default') ? 'id' : 'name';
+
+const sites = {
+  sharepoint: {
+    baseUrl: `https://${domain}.sharepoint.com/SitePages${key ? '/' + key : ''}`,
+    spaceChar: '%20'
+  },
+  confluence: {
+    baseUrl: `https://confluence.${domain.match(/\.\w{2,}$/) ? domain : domain + '.com'}/display/${key}`,
+    spaceChar: '+'
+  },
+  custom: {
+    baseUrl: `https://${domain}/${key}`,
+    spaceChar
+  },
+  default: {
+    baseUrl: `https://apps.us.jupiterone.io/policies`,
+    spaceChar: ''
+  }
+};
 
 const config = JSON.parse(fs.readFileSync(configFile));
+const siteConfig = sites[site.toLowerCase()];
+
+if (!siteConfig) {
+  error.fatal(`Unsupported site: ${site}`);
+}
 
 const mapping = {};
 
@@ -28,9 +71,13 @@ for (const policy of config.policies || []) {
 
 for (const procedure of config.procedures || []) {
   const id = procedure.id;
-  const name = procedure.name;
   const policy = mapping[id];
-  procedure.webLink = `${baseUrl}/${policy.name.replace(/\s/g, '+')}/${name.replace(/\s/g, '+')}`;
+
+  procedure.webLink = `${siteConfig.baseUrl}/${
+    policy[param].replace(/\s/g, siteConfig.spaceChar)
+  }/${
+    procedure[param].replace(/\s/g, siteConfig.spaceChar)
+  }`;
 }
 
 fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
