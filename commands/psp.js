ababda2b9c2eda4857a88986b8331cef3aa9edcf
erkangz <erkang.zheng@lifeomic.com>
2020-01-14 09:28:42 -0500

    Add sub-commands to bin

--- commands/psp.js
+++ commands/psp.js
@@ -3,7 +3,7 @@
 const program = require('commander');
 
 program
-  .version(require('../package').version, '-V, --version')
+  .version(require('../package').version, '-v, --version')
   .command('build', 'build PSP markdown documentation from templates')
   .command('assess', 'generate compliance self-assessment markdown report')
   .command('publish', 'upload PSP assets to JupiterOne')
